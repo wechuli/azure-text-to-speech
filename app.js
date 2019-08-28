43446307ec1b1350c9c359cbbedcd0879910a58b
Wechuli <wechulipaul@yahoo.com>
2019-08-28 19:16:21 +0300

    comment on key

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { getAccessToken, textToSpeech } = require("./workers");
 const readline = require("readline-sync");
 
-const subscriptionKey = process.env.COGNITIVE_SERVICE_KEY;
+const subscriptionKey = process.env.COGNITIVE_SERVICE_KEY; // get your OWN Key
 
 const textString = "And you as well must die beloved dust and all your beauty stand you in no stead; This flawless, vital hand, this perfect head, This body of flame and steel, before the gust Of Death, or under his autumnal frost, Shall be as any leaf, be no less dead Than the first leaf that fell,—this wonder fled. Altered, estranged, disintegrated, lost. Nor shall my love avail you in your hour. In spite of all my love, you will arise Upon that day and wander down the air Obscurely as the unattended flower, It mattering not how beautiful you were,Or how beloved above all else that dies."
 
